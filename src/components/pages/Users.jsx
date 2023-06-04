a2e1620f81e8382bb5c5b348d2e3035ff787aa85
渡邉　展夢 <86395724+Hiromu-Watanabe@users.noreply.github.com>
2023-06-04 17:09:28 +0000

    fix: レンダリング最適化 summary: グローバルなstate管理での不要な子コンポーネントのレンダリングをmemo化で回避

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 
 import { SearchInput } from "../molcules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { UserContext } from "../../providers/UserProvider";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -20,10 +22,17 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const onClickSwitch = () =>
+    setUserInfo({ isAdmin: userInfo ? !userInfo.isAdmin : false });
+
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput></SearchInput>
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user}></UserCard>
